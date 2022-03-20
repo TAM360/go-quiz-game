8ddfb7d67908e635f6b51d51a711ad8fee71ebbf
Taimoor Mirza <demon6656@gmail.com>
2022-03-20 12:33:22 +0500

    Add stop watch CLI. Flag

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func getUserInput(ch chan string, q QuizQuestion) {
 	ch <- answer
 }
 
-func quizGame(csvFileName string) (uint, uint, int) {
+func quizGame(csvFileName string, quizTime int) (uint, uint, int) {
 	questions := readCsvFile(csvFileName)
 	ch := make(chan string, 1)
 	breakLoop := false
@@ -84,7 +84,7 @@ func quizGame(csvFileName string) (uint, uint, int) {
 				correctAnswers++
 			}
 			remainingQuestions -= 1
-		case <-time.After(10 * time.Second):
+		case <-time.After(time.Duration(quizTime) * time.Second):
 			fmt.Println("Time complete.")
 			breakLoop = true
 		}
@@ -94,21 +94,23 @@ func quizGame(csvFileName string) (uint, uint, int) {
 }
 
 func main() {
-	var defaultFile string = "problems.csv"
+	defaultFile := "problems.csv"
+	defaultTime := 30
 	var correctAnswers uint = 0
 	var wrongAnswers uint = 0
 	var remainingQuestions int = 0
-	var fileFlag = flag.String("file-name", defaultFile, "Name of the CSV file. Default is "+defaultFile)
+	var fileFlag = flag.String("file-name", defaultFile, "Name of the CSV file. Default is "+defaultFile+".")
+	var timerFlag = flag.Int("timer", defaultTime, "Time of the quiz. Default value is 30 seconds.")
 
 	flag.Parse()
 
 	if fileExists(*fileFlag) {
-		correctAnswers, wrongAnswers, remainingQuestions = quizGame(*fileFlag)
+		correctAnswers, wrongAnswers, remainingQuestions = quizGame(*fileFlag, *timerFlag)
 		fmt.Println("Quiz Stats")
 		fmt.Printf("Right answers: %v\n", correctAnswers)
 		fmt.Printf("Wrong answers: %v\n", wrongAnswers)
 		fmt.Printf("Not answered: %v\n", remainingQuestions)
-		
+
 	} else {
 		fmt.Println("File doesn't exists")
 	}
